82b9d6bb1024f8c43b883ce89ece4b2a5395f791
Jamie Taylor <jvptaylor@hotmail.com>
2023-09-24 22:53:36 -0400

    Initial commit Initial

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,22 @@ import Head from 'next/head';
 import Footer from '../components/Footer';
 import PostCard from '../components/PostCard';
 import { getAllPosts } from '../lib/test-data';
+import { client } from '../lib/apollo';
+import { gql } from '@apollo/client';
 
 
 export default function Home({ posts }) {
   return (
     <div className="container">
       <Head>
-        <title>Headless WP Next Starter</title>
+        <title>Htmyell Headless WP</title>
         <link rel="icon" href="favicon.ico"></link>
       </Head>
 
       <main>
         <h1 className="title">
-          Headless WordPress Next.js Starter
+          Htmyell Design & Development
         </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
 
         <div className="grid">
           {
@@ -39,8 +37,19 @@ export default function Home({ posts }) {
 
 export async function getStaticProps(){
 
-  const response = await getAllPosts()
-  const posts = response?.data?.posts?.nodes
+  const GET_POSTS = gql`query getPosts {
+      posts {
+        nodes {
+          title
+          content
+          uri
+          date
+        }
+      }
+    }`;
+  const response = await client.query({ query: GET_POSTS });
+  const posts = response?.data?.posts?.nodes;
+
   return {
     props: {
       posts
